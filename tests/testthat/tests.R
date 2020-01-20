d428a97f330cdee02946d82331ece07b7e18a964
Martin Hinz <martin.hinz@ufg.uni-kiel.de>
2020-01-20 12:28:27 +0100

    included render_plots in tests

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -6,12 +6,25 @@ test_that("run_analysis runs proper", {
     NA)
 })
 
-test_that("run_analysis runs proper", {
-  result <- readRDS(system.file("testdata", "result.rds", package = "sensitivity.sumcal.article.2020"))
+result <- readRDS(system.file("testdata", "result.rds", package = "sensitivity.sumcal.article.2020"))
+
+test_that("render_tables runs proper", {
   expect_error(
     {render_orig_sim_result_table(result$orig_sim)},
     NA)
   expect_error(
     {render_full_sim_result_table(result$full_sim)},
+    NA)
+})
+
+test_that("render_plots runs proper", {
+  expect_error(
+    {render_orig_sim_result_boxplot(result$orig_sim)},
+    NA)
+  expect_error(
+    {render_orig_sim_result_regression(result$orig_sim)},
+    NA)
+  expect_error(
+    {render_full_sim_result_regression(result$full_sim)},
     NA)
 })
